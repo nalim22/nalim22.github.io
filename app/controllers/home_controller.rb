58faa1de8428189804b38f7b87033f8a1f20e5a7
nong_nong@likelion.org <nong_nong@likelion.org>
2018-04-09 09:16:55 +0000

    init

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,13 @@
 class HomeController < ApplicationController
     def index
         #index action을 생성
-        num = 1..45
-        @lotto = num.to_a.sample(6)
+        # num = 1..45
+        # @lotto = num.to_a.sample(6)
         
-        text = ["a","b","c","d","e","f","g"]
-        @text = text.sample(2)
+        text = ["귀찮음을 조금 넣어야징..!","식욕을 넣어볼까","..흠 딱히 넣을게 없넹","낯가림을 넣어볼까","집순이 본능을 넣어볼까"]
+        @text = text.sample(3)
         
         
+     
     end
 end
